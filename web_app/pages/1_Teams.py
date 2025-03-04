1ecacdd395e6f9b383fc7a29fdac94d895f76369
J-Emma <jeanne-emma@kinetix.tech>
2025-03-04 16:53:19 +0100

    :seedling: (home/team page) Update the home page to remove the stremlit content and add a team page

---
+++ web_app/pages/1_Teams.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Page Title
+st.title("Teams")
+
+st.markdown("""
+               Les membres de l'équipe sont : Abdelhalim, Fabien, Jeanne-Emma et Leïla
+                """)
